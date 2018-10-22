9932f50e08403f87b6ccdf5b40cba2dc29f67d2c
sotowang <sotowang@qq.com>
2018-10-22 21:24:54 +0800

    Spark Streaming 处理文件数据

---
+++ src/main/scala/com/soto/spark/sparkstreaming/FileWordCount.scala
@@ -0,0 +1,24 @@
+package com.soto.spark.sparkstreaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+  * 使用Spark Streaming处理文件系统（local/hdfs）的数据
+  */
+object FileWordCount {
+  def main(args: Array[String]): Unit = {
+    val sparkConf = new SparkConf().setMaster("local").setAppName("FileWordCount")
+
+    val ssc = new StreamingContext(sparkConf,Seconds(5))
+
+    val lines = ssc.textFileStream("file:///home/sotowang/user/note/other")
+
+    val result = lines.flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_)
+
+    result.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
